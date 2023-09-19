1ee99df20bac11892d2160e2a86fae9edca3eacf
knapp <emiliodamasco04@gmail.com>
2023-09-19 06:37:07 -0700

    added 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let count = 0;
+
+require('request')(process.argv[2], (e, r, data) => {
+    for (const movie of JSON.parse(data).results) {
+        for (const char of movie.characters) {
+            if (char.includes('18')) {
+                count++;
+            }
+        }
+    }
+    console.log(count);
+});
