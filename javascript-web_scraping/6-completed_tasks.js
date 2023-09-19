c3c83830f399d9068a8808c5626597750575bcbb
knapp <emiliodamasco04@gmail.com>
2023-09-19 07:28:28 -0700

    fixed uncompleted tasks showing

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -11,7 +11,7 @@ require('request')(process.argv[2], (e, r, data) => {
     for (const i of JSON.parse(data)) {
         if (i.completed) { dict[i.userId] += 1; }
     }
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < Object.dict(keys).length; i++) {
         if (dict[`${i + 1}`] === 0) { delete dict[`${i + 1}`]; }
     }
     console.log(dict);
