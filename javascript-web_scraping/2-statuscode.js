23e4bc8b79eecfe95abbbfbd99d6cac36deffe14
knapp <emiliodamasco04@gmail.com>
2023-09-19 06:16:41 -0700

    added 3

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,3 +3,4 @@
 require('request').get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
+
---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+require('request')("https://swapi-api.hbtn.io/api/films/" + process.argv[2], (e, r, data) => {
+    e ? console.error(e) : console.log(JSON.parse(data).title);
+});
