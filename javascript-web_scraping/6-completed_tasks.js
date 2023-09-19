2aa44efcb574c286f4826a4a1d63f60a0f4bb2b6
knapp <emiliodamasco04@gmail.com>
2023-09-19 07:35:37 -0700

    fixed dict length

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,15 +4,15 @@ const arr = [];
 const dict = {};
 
 require('request')(process.argv[2], (e, r, data) => {
-    for (const i of JSON.parse(data)) {
-        if (!arr.includes(i.userId)) { arr.push(i.userId); }
-    }
-    for (let key = 1; key <= arr.length; key++) { dict[key] = 0; }
-    for (const i of JSON.parse(data)) {
-        if (i.completed) { dict[i.userId] += 1; }
-    }
-    for (let i = 0; i < arr.length; i++) {
-        if (dict[`${i + 1}`] === 0) { delete dict[`${i + 1}`]; }
-    }
-    console.log(dict);
+  for (const i of JSON.parse(data)) {
+    if (!arr.includes(i.userId)) { arr.push(i.userId); }
+  }
+  for (let key = 1; key <= arr.length; key++) { dict[key] = 0; }
+  for (const i of JSON.parse(data)) {
+    if (i.completed) { dict[i.userId] += 1; }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (dict[`${i + 1}`] === 0) { delete dict[`${i + 1}`]; }
+  }
+  console.log(dict);
 });
