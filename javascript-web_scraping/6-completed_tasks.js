7cc7b62a9e2e3a7e5f68e7f3b4c7d89134e775f5
knapp <emiliodamasco04@gmail.com>
2023-09-19 07:27:22 -0700

    test

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -11,7 +11,7 @@ require('request')(process.argv[2], (e, r, data) => {
     for (const i of JSON.parse(data)) {
         if (i.completed) { dict[i.userId] += 1; }
     }
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 0; i < 10; i++) {
         if (dict[`${i + 1}`] === 0) { delete dict[`${i + 1}`]; }
     }
     console.log(dict);
